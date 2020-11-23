be272e2e8e665835602557a5fcde9fdb604489e8
Administrator <admin@example.com>
2020-11-23 13:44:06 +0000

    Instructor Commit

---
+++ src/main/java/oop/hw/Main.java
@@ -0,0 +1,17 @@
+package oop.hw;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Person person = new Person();
+    Student student = new Student();
+    Teacher teacher = new Teacher();
+    
+    System.out.println(person.run());
+    System.out.println(student.run());
+    System.out.println(teacher.run());
+    
+  }
+
+}
